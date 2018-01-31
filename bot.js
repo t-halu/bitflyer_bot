ac274cbdae5df3fd035539851f033eb37df325f5
研究室デスクトップ <tajima@ems.k.u-tokyo.ac.jp>
2018-01-31 21:34:05 +0900

    api呼び出し用の関数を作成

--- bot.js
+++ bot.js
@@ -1,29 +1,32 @@
 var request = require('request');
-var crypto=require('crypto');
+var crypto = require('crypto');
 var fs = require('fs');
 var bitflyer_apikey = JSON.parse(fs.readFileSync('C:/bitflyer_apikey.json', 'utf8'))
 //gitにあげたくないので秘密鍵はCドライブ直下においとく
 const ApiKey = bitflyer_apikey.apikey;
 const ApiSecret = bitflyer_apikey.apisecret;
 const API_VERSION = '/V1';
-var timestamp = Date.now().toString();
-var method = 'GET';
-var path = '/me/getaddresses';
-var text = timestamp + method + API_VERSION + path;
-var sign = crypto.createHmac('sha256', ApiSecret).update(text).digest('hex');
 
-var options = {
-  url: 'https://api.bitflyer.jp' + API_VERSION + path,
-  method: method,
-  headers: {
-    'ACCESS-KEY': ApiKey,
-    'ACCESS-TIMESTAMP': timestamp,
-    'ACCESS-SIGN': sign,
-    'Content-Type': 'application/json'
-  }
+function call(method, path, body, callback) {
+  var timestamp = Date.now().toString();
+  var text = timestamp + method + API_VERSION + path + body;
+  var sign = crypto.createHmac('sha256', ApiSecret).update(text).digest('hex');
+  var url = 'https://api.bitflyer.jp' + API_VERSION + path;
+  var options = {
+    url: url,
+    method: method,
+    body: body,
+    headers: {
+      'ACCESS-KEY': ApiKey,
+      'ACCESS-TIMESTAMP': timestamp,
+      'ACCESS-SIGN': sign,
+      'Content-Type': 'application/json'
+    }
+  };
+  request(options, callback);
 }
+//汎用的なAPI呼び出し関数
 
-
-request(options, function(err, response, body) {
+call('GET','/me/getaddresses','', function(err, response, body) {
   console.log(body);
 });
