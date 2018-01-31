be81ef2c834e178f67597d47c58f0d9301cf91d1
研究室デスクトップ <tajima@ems.k.u-tokyo.ac.jp>
2018-01-31 22:30:10 +0900

    レスポンスの中身を取り出したい

--- bot.js
+++ bot.js
@@ -35,8 +35,61 @@ function getMarkets(callback) {
     callback();
   }
 }
-//マーケットの一覧を呼び出し
+/*マーケットの一覧を呼び出し
+[
+  { "product_code": "BTC_JPY" },
+  { "product_code": "FX_BTC_JPY" },
+  { "product_code": "ETH_BTC" },
+  {
+    "product_code": "BTCJPY28APR2017",
+    "alias": "BTCJPY_MAT1WK"
+  },
+  {
+    "product_code": "BTCJPY05MAY2017",
+    "alias": "BTCJPY_MAT2WK"
+  }
+]*/
 
-getMarkets(function() {
-  console.log('完了')
-});
+function getBoard(callback) {
+  var mid_price;
+  call('GET', '/getboard', '', function(err, response, body) {
+    //console.log(body);
+    mid_price=JSON.parse(body).mid_price;
+    console.log(mid_price);
+  });
+  if (callback) {
+    callback();
+  }
+  console.log(mid_price);
+  return mid_price;
+
+}
+/*板情報を呼び出し
+{
+  "mid_price": 33320,
+  "bids": [
+    {
+      "price": 30000,
+      "size": 0.1
+    },
+    {
+      "price": 25570,
+      "size": 3
+    }
+  ],
+  "asks": [
+    {
+      "price": 36640,
+      "size": 5
+    },
+    {
+      "price": 36700,
+      "size": 1.2
+    }
+  ]
+}*/
+
+/*getMarkets(function() {
+});*/
+
+console.log(getBoard());
