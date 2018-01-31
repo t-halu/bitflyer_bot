212ad29c3414f52e4976f683e86ba3a8e962ff9b
研究室デスクトップ <tajima@ems.k.u-tokyo.ac.jp>
2018-01-31 15:23:22 +0900

    公式のコピペ

--- bot.js
+++ bot.js
@@ -1,1 +1,7 @@
-console.log('hello world!');
+var request = require('request');
+var path = '/v1/getmarkets';
+var query = '';
+var url = 'https://api.bitflyer.jp' + path + query;
+request(url, function(err, response, payload) {
+  console.log(payload);
+});
