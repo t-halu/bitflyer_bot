6fa4ddc4e5818f2a69d29a556e4244d373d57e03
研究室デスクトップ <tajima@ems.k.u-tokyo.ac.jp>
2018-01-31 20:27:35 +0900

    apikeyを読み込めるようにした（秘密鍵はgitにおかない）

--- bot.js
+++ bot.js
@@ -1,8 +1,17 @@
 var request = require('request');
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('C:/bitflyer_apikey.json', 'utf8'))
+//gitにあげたくないので秘密鍵はCドライブ直下においとく
+const ApiKey = config.apikey;
+const ApiSecret = config.apisecret;
 const API_VERSION='/V1';
-var path = '/getmarkets';
+
+console.log(ApiKey);
+
+/*var path = '/getmarkets';
 var url = 'https://api.bitflyer.jp' + API_VERSION + path;
 request(url, function(err, response, body) {
   console.log(response);
   console.log(body);
 });
+*/
